4d7084452a2973ab9d79c8f449caec5cc11f2f03
Aozora09 <azharirambe2003@gmail.com>
2023-05-24 20:43:35 +0800

    Tugas Latihan PMB

---
+++ script.js
@@ -0,0 +1,19 @@
+//Klik Gambar ke Link
+function submitform(){
+  alert("Pesan Sudah Dikirim");
+}
+function OpenmyIG() {
+  // Logika atau aksi tambahan saat gambar diklik
+  window.open('https://www.instagram.com/azhari_aozora/?igshid=ZDc4ODBmNjlmNQ%3D%3D');
+}
+function OpenDhafgit() {
+  // Logika atau aksi tambahan saat gambar diklik
+  window.open('https://github.com/not-xygen');
+}
+function OpenDhafIG() {
+  // Logika atau aksi tambahan saat gambar diklik
+  window.open('https://instagram.com/dhaffaagus_?igshid=MmIzYWVlNDQ5Yg==');
+}
+function openlink(){
+  window.open('https://www.youtube.com/@HajiKoding')
+}
